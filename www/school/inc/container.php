4864e48997c6dea96bff9ae5121b96b88ef9ba97
MOB-MKO <oladegamukaram@gmail.com>
2024-03-09 06:41:37 -0800

    'Testing'

---
+++ www/school/inc/container.php
@@ -0,0 +1,25 @@
+<link rel="icon" type="" href="">
+</head>
+<body class="">
+<div role="navigation" class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button data-target=".navbar-collapse" data-toggle="collapse" class="navbar-toggle" type="button">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a href="http://192.168.33.99/school/dashboard.php" class="navbar-brand">PURPLESTUDIO.COM</a>
+        </div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="http://192.168.33.99/school/dashboard.php">Home</a></li>
+
+          </ul>
+
+        </div><!--/.nav-collapse -->
+      </div>
+    </div>
+
+	<div class="container" style="min-height:500px;">
